9e102bce5e54788366a7c07289de252d141830d9
araxyso <69326871+Zinedinarnaut@users.noreply.github.com>
2023-12-23 12:57:39 +1100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware to enable CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://hanime-api-five.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', true);
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
