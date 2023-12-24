c7e09bb8a9fab389ac99011ee752a5336497e914
araxyso <69326871+Zinedinarnaut@users.noreply.github.com>
2023-12-24 12:01:20 +1100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,15 +6,6 @@ const cors = require('cors');
 
 const app = express();
 
-// Middleware to enable CORS
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', true);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-
 app.use(cors());
 
 const jsongen = async (url) => {
